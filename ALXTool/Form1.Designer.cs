2914c444e575b09d7767fb130bb87a991bb49805
lm3515@qq.com <lm3515@qq.com>
2021-10-19 13:59:33 +0800

    1.2
    云盒升级工具
    
    Co-Authored-By: Richard Liu <lm3515@qq.com>

---
+++ ALXTool/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ALXTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.logArea = new System.Windows.Forms.RichTextBox();
+            this.label_connectStatus = new System.Windows.Forms.Label();
+            this.button_ConnectPrintBox = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // logArea
+            // 
+            this.logArea.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.logArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logArea.Font = new System.Drawing.Font("新宋体", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.logArea.Location = new System.Drawing.Point(13, 13);
+            this.logArea.Name = "logArea";
+            this.logArea.ReadOnly = true;
+            this.logArea.Size = new System.Drawing.Size(713, 431);
+            this.logArea.TabIndex = 0;
+            this.logArea.Text = "";
+            // 
+            // label_connectStatus
+            // 
+            this.label_connectStatus.AutoSize = true;
+            this.label_connectStatus.BackColor = System.Drawing.Color.Red;
+            this.label_connectStatus.Location = new System.Drawing.Point(574, 480);
+            this.label_connectStatus.Name = "label_connectStatus";
+            this.label_connectStatus.Size = new System.Drawing.Size(11, 12);
+            this.label_connectStatus.TabIndex = 11;
+            this.label_connectStatus.Text = " ";
+            // 
+            // button_ConnectPrintBox
+            // 
+            this.button_ConnectPrintBox.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_ConnectPrintBox.Location = new System.Drawing.Point(591, 450);
+            this.button_ConnectPrintBox.Name = "button_ConnectPrintBox";
+            this.button_ConnectPrintBox.Size = new System.Drawing.Size(135, 58);
+            this.button_ConnectPrintBox.TabIndex = 12;
+            this.button_ConnectPrintBox.Text = "开始升级";
+            this.button_ConnectPrintBox.UseVisualStyleBackColor = true;
+            this.button_ConnectPrintBox.Click += new System.EventHandler(this.button_ConnectPrintBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 450);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "前置条件：将升级包 upgrade.fac放在 C:\\tmp文件夹下";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 468);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 12);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "电脑连接盒子WiFi，不需要给盒子配网";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 487);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 12);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "准备好后直接点击开始升级";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 517);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_ConnectPrintBox);
+            this.Controls.Add(this.label_connectStatus);
+            this.Controls.Add(this.logArea);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "云盒升级工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox logArea;
+        private System.Windows.Forms.Label label_connectStatus;
+        private System.Windows.Forms.Button button_ConnectPrintBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ALXTool/Form1.cs
@@ -0,0 +1,220 @@
+﻿using Newtonsoft.Json;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinSCP;
+
+namespace ALXTool
+{
+    public partial class Form1 : Form
+    {
+        bool isOld = true;
+        SshClient sshClient;//= new SshClient("192.168.18.1", "dg", "ivanlee");
+        string ip;
+        string username;
+        string password;
+        StreamWriter successList = new StreamWriter("C:\\tmp\\successList.txt",true);
+
+        public Form1()
+        {
+            successList.AutoFlush = true;
+            InitializeComponent();
+            string name = Dns.GetHostName();
+            IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
+            foreach (IPAddress ipa in ipadrlist)
+            {
+                if (ipa.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    logArea.AppendText("本机IP：" + ipa.ToString() + "\n");
+                    if (ipa.ToString().StartsWith("192.168.11"))// 旧固件
+                    {
+                        isOld = true;
+                        ip = "192.168.11.123";
+                        username = "root";
+                        password = "admin";
+                    }
+                    else
+                    {
+                        isOld = false;
+                        ip = "192.168.18.1";
+                        username = "dg";
+                        password = "ivanlee";
+                    }
+                    sshClient = new SshClient(ip, username, password);
+                }
+            }
+        }
+
+
+    private void button_ConnectPrintBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                button_ConnectPrintBox.Enabled = false;
+                //logArea.AppendText("正在连接盒子...\n");
+                if (sshClient.IsConnected)
+                {
+                    button_ConnectPrintBox.Enabled = true;
+                    return;
+                }
+
+                logArea.Clear();
+                logArea.AppendText("正在连接盒子...\n");
+
+                int i = 0;
+                while (!sshClient.IsConnected)
+                {
+                    try
+                    {
+                        sshClient.Connect();
+                    }
+                    catch
+                    {
+                        logArea.AppendText("正在连接盒子...\n");
+                    }
+                    if (++i >= 10)
+                    {
+                        MessageBox.Show("连接盒子失败！");
+                        button_ConnectPrintBox.Enabled = true;
+                        return;
+                    }
+                }
+                logArea.AppendText("盒子连接成功\n");
+                label_connectStatus.BackColor = Color.Green;
+
+                SshCommand sshCommand = sshClient.RunCommand("hexdump /dev/mtd3 -C -s 1024 -n 16 |head -1 |awk  -F \"|\" '{print $2}' |tr -d \".\"");
+                string imei = sshCommand.Result.Trim();
+                logArea.AppendText("IMEI: "); logArea.AppendText(imei); logArea.AppendText("\n");
+
+                SshCommand sshCommand2 = sshClient.RunCommand("hexdump /dev/mtd3 -C -s 4 -n 6 |head -1 |awk  -F  \" \" '{print $2\":\"$3\":\"$4\":\"$5\":\"$6\":\"$7}'");
+                string mac = sshCommand2.Result.Trim();
+                logArea.AppendText("Mac地址: "); logArea.AppendText(mac); logArea.AppendText("\n");
+
+                // 保存IMEI和Mac地址
+                string infoJson = @"C:\tmp\deviceInfo.json";
+                if (File.Exists(infoJson))
+                    File.Delete(infoJson);
+                Dictionary<string, string> deviceInfo = new Dictionary<string, string>();
+                deviceInfo.Add("imei", imei);
+                deviceInfo.Add("mac", mac);
+                string json = JsonConvert.SerializeObject(deviceInfo);
+                File.WriteAllText(infoJson, json);
+
+                // 删除缓存密钥
+                sshClient.RunCommand("uci delete wireless.@wifi-iface[0].YJSecret");
+
+                if (!Upload())
+                {
+                    sshClient.Disconnect();
+                    label_connectStatus.BackColor = Color.Red;
+                    button_ConnectPrintBox.Enabled = true;
+                    return;
+                }
+
+                logArea.AppendText("擦除中...\n");
+                sshClient.RunCommand("mtd erase ALL");
+                logArea.AppendText("擦除完成\n");
+                logArea.AppendText("正在烧录...\n");
+                sshClient.RunCommand("mtd write /tmp/upgrade.fac ALL");
+                logArea.AppendText("烧录成功\n");
+                logArea.AppendText("IMEI回写...\n");
+                //sshClient.RunCommand("echo -en "+ imei +" | dd of=/dev/mtdblock3 bs=1 seek=1024 conv=notrunc");
+                //SshCommand result3 =  sshClient.RunCommand("echo  "+ mac +" | sed 's/:/\\\\x/g' |sed -e 's/^/\\\\x/'");
+                //string mac2 = result3.Result;
+                //sshClient.RunCommand("echo -en "+ mac2 +" |  dd of=/dev/mtdblock3 bs=1 seek=4 conv=notrunc");
+                ///opt/bin/xfwu_imei_uevent.sh 800200806100536H  68:8f:c9:f0:11:10
+                sshClient.RunCommand("/opt/bin/xfwu_imei_uevent.sh " + imei + " " + mac);
+
+                SshCommand sshCommand4 = sshClient.RunCommand("hexdump /dev/mtd3 -C -s 1024 -n 16 |head -1 |awk  -F \"|\" '{print $2}' |tr -d \".\"");
+                string imei2 = sshCommand4.Result.Trim();
+                logArea.AppendText("IMEI: "); logArea.AppendText(imei2); logArea.AppendText("\n");
+                SshCommand sshCommand5 = sshClient.RunCommand("hexdump /dev/mtd3 -C -s 4 -n 6 |head -1 |awk  -F  \" \" '{print $2\":\"$3\":\"$4\":\"$5\":\"$6\":\"$7}'");
+                string mac3 = sshCommand5.Result.Trim();
+                logArea.AppendText("Mac地址: "); logArea.AppendText(mac3); logArea.AppendText("\n");
+
+                logArea.AppendText("写入成功\n");
+
+                successList.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " - " + imei2);
+
+                logArea.AppendText("升级成功，请等待盒子重启完成！\n");
+                sshClient.RunCommand("reboot");
+                sshClient.Disconnect();
+                label_connectStatus.BackColor = Color.Red;
+                button_ConnectPrintBox.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("更新失败！");
+                sshClient.Disconnect();
+                label_connectStatus.BackColor = Color.Red;
+                button_ConnectPrintBox.Enabled = true;
+            }
+        }
+
+        private bool Upload()
+        {
+            logArea.AppendText("准备推送固件...\n");
+            
+            SessionOptions sessionOptions = new SessionOptions
+            {
+                Protocol = Protocol.Scp,
+                //HostName = "192.168.11.123",
+                //UserName = "root",
+                //Password = "admin",
+                HostName = ip,
+                UserName = username,
+                Password = password,
+                //SshHostKeyFingerprint = ftpSSHFingerPrint
+                GiveUpSecurityAndAcceptAnySshHostKey = true
+            };
+
+            string sourcefilepath = "C:\\tmp\\upgrade.fac";
+            string filename = Path.GetFileName(sourcefilepath); 
+            string ftpfullpath = "/tmp/" + filename;
+
+            using (WinSCP.Session session = new WinSCP.Session())
+            {
+                try
+                {
+                    // Connect
+                    session.Open(sessionOptions);
+                    logArea.AppendText("正在推送...\n");
+
+                    // Upload files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+
+                    TransferOperationResult transferResult = session.PutFiles(sourcefilepath, ftpfullpath, false, transferOptions);
+                    if (!transferResult.IsSuccess)
+                    {
+                        MessageBox.Show("固件推送失败！");
+                        return false;
+                    }
+                    logArea.AppendText("固件推送成功\n");
+                }
+                catch
+                {
+                    MessageBox.Show("固件推送失败！");
+                    return false;
+                }
+                
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
